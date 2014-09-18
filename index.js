fea681dd34fe8ef27f3794a6506b6174df3c9b60
Artem <artofkot@gmail.com>
2014-09-17 21:29:38 -0400

    another try

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express();
+var fs=require('fs');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-var buffer = new Buffer(fs.readFile("index.html"),"utf-8");
+var buffer = new Buffer(fs.readFileSync("index.html"),"utf-8");
 
 app.get('/', function(request, response) {
   response.send(buffer.toString("utf-8", 0, 12))
