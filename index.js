81f5f258320e41e9b54a91776fd2b68279a05c00
Artem <artofkot@gmail.com>
2014-09-17 21:13:59 -0400

    adding buffer

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buffer = new Buffer(fs.readFile("index.html"),"utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(buffer.toString("utf-8", 0, 12))
 })
 
 app.listen(app.get('port'), function() {
